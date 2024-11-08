cf3641fb170ebd6136fcf8f3ba1d1301a424af0d
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-11-08 15:05:55 +0530

    Setup Room Database

---
+++ app/src/main/java/com/example/taskmaster/model/Task.kt
@@ -0,0 +1,15 @@
+package com.example.taskmaster.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "tasks")
+data class Task(
+
+    @PrimaryKey(autoGenerate = true) val id:Int=0,
+    val title:String,
+    val description:String,
+    val isCompleted:Boolean=false,
+    val timestamp:Long=System.currentTimeMillis()
+)
