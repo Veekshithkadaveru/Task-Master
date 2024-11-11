a73f02b4e29c4533707297de4fc7ec5fe76508db
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-11-11 19:57:35 +0530

    Initialize MainActivity with Hilt dependency injection and navigation setup

--- app/src/main/java/com/example/taskmaster/MainActivity.kt
+++ app/src/main/java/com/example/taskmaster/MainActivity.kt
@@ -3,44 +3,38 @@ package com.example.taskmaster
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
 import com.example.taskmaster.ui.theme.TaskMasterTheme
+import com.example.taskmaster.viewmodel.TaskViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TaskMasterTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                TaskMasterApp()
             }
+        }
+    }
+
+    @Composable
+    fun TaskMasterApp() {
+        rememberNavController()
+
+        val taskViewModel: TaskViewModel = hiltViewModel()
+
+        Surface(color = MaterialTheme.colorScheme.background) {
+            AppNavigation(viewModel = taskViewModel)
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TaskMasterTheme {
-        Greeting("Android")
-    }
-}
