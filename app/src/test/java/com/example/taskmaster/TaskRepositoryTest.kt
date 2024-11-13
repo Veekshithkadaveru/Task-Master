367db9855f5814a2d07ced886c81af226f6dfb24
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-11-13 22:48:27 +0530

    Add Unit test for repository to ensure it returns flow of tasks

---
+++ app/src/test/java/com/example/taskmaster/TaskRepositoryTest.kt
@@ -0,0 +1,36 @@
+package com.example.taskmaster
+
+import com.example.taskmaster.model.Task
+import com.example.taskmaster.model.TaskDao
+import com.example.taskmaster.repository.TaskRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class TaskRepositoryTest {
+
+    private lateinit var taskRepository: TaskRepository
+    private val taskDao: TaskDao = mockk()
+
+    @Before
+    fun setUp() {
+        taskRepository = TaskRepository(taskDao)
+    }
+
+    @Test
+    fun `verify getTasks returns flow of tasks`()= runTest {
+
+        val taskList = listOf(Task(id = 1, title = "Test Task", description = "Test Description"))
+        coEvery { taskDao.getTasks() } returns flowOf(taskList)
+
+        val tasksFlow = taskRepository.getTasks()
+
+        tasksFlow.collect { tasks ->
+            assertEquals(taskList, tasks)
+        }
+    }
+}
